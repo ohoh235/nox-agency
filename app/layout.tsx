96edd8f8f9f7220b608a33ca9540eac56e4b9872
ohoh235 <ruslanezov88@gmail.com>
2026-04-11 01:02:24 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,4 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="ru">
       <body style={{ margin: 0 }}>{children}</body>
